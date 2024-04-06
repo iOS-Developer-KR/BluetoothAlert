5af61d8ef853e7793317682f740168022214ad93
happyytw <happyytw@gmail.com>
2024-04-07 00:02:33 +0900

    Initial Commit

---
+++ BluetoothAlert/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BluetoothAlert
+//
+//  Created by Taewon Yoon on 4/7/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
